1b30714854c231ba1bad6115d54b1f645942f31e
Noel Mendoza <nmendozavd@gmail.com>
2019-05-08 05:46:18 -0700

    updated with readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Coding Dojo Bootcamp Assignment  
+## Python, Django / Python / Find Characters
+
+### Submit Files
+```
+findcharacters.py
+```
+
+### Assignment details  
+Write a program that takes a list of strings and a string containing a single character, and prints a new list of all the strings containing that character.  
+
+Here's an example:  
+
+```
+# input
+word_list = ['hello','world','my','name','is','Anna']
+char = 'o'
+# output
+new_list = ['hello','world']
+```
+
+Hint: how many loops will you need to complete this task?
+
--- findcharacters.py
+++ findcharacters.py
@@ -1,12 +1,19 @@
-word_list = ['hello','world','my','name','is','Anna']
+word_list = ['hello', 'world', 'my', 'name', 'is', 'Anna']
 char = 'o'
-def find_character(world_list, char):
+
+
+def find_character(word_list, char):
     new_list = []
+
     for i in range(0, len(word_list)):
         if word_list[i].find(char):
             new_list.append(word_list[i])
 
     print new_list
+    
+
+
+
 
 
     # def find_character(word_list, char):
@@ -17,3 +24,5 @@ def find_character(world_list, char):
     #         new_list.append(word_list[i])
     #
     # print new_list
+
+
